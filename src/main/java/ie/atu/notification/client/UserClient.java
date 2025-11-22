70d24b82bb8b14f42f9322c8b046b63f57471bbd
sighman <g00286398@atu.ie>
2025-11-22 12:08:46 +0000

    Adding Welcome Email to trigger on new user creation

--- src/main/java/ie/atu/notification/client/UserClient.java
+++ src/main/java/ie/atu/notification/client/UserClient.java
@@ -1,8 +1,14 @@
 package ie.atu.notification.client;
 
 
+import ie.atu.notification.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "service-user", url = "http://localhost:8080")
 public interface UserClient {
+
+    @GetMapping("/users/{id}")
+    UserDto getByUserId(@PathVariable("id") String id);
 }
--- src/main/java/ie/atu/notification/service/NotificationService.java
+++ src/main/java/ie/atu/notification/service/NotificationService.java
@@ -1,9 +1,41 @@
 package ie.atu.notification.service;
 
+import ie.atu.notification.client.PaymentClient;
+import ie.atu.notification.client.UserClient;
+import ie.atu.notification.dto.UserDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+
+  private final  UserClient userClient;
+  private final PaymentClient paymentClient;
+  private final EmailService emailService;
+
+
+  public NotificationService(UserClient userClient, PaymentClient paymentClient, EmailService emailService) {
+    this.userClient = userClient;
+    this.paymentClient = paymentClient;
+    this.emailService = emailService;
+  }
+
+
+
+  public void WelcomeEmail(String userId) {
+      UserDto user = userClient.getByUserId(userId);
+
+
+      emailService.sendEmail(
+              user.getEmail(),
+              "Welcome!",
+              "Hi " + user.getName() + ", welcome to our platform!"
+      );
+
+
+
+  }
+
+
 
 
 
