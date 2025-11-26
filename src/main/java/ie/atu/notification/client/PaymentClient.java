619bd3468c2beda5922e513f209e31654892c022
sighman <g00286398@atu.ie>
2025-11-26 13:10:48 +0000

    Adding port number and pdf service.

--- src/main/java/ie/atu/notification/client/PaymentClient.java
+++ src/main/java/ie/atu/notification/client/PaymentClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "service-payment", url = "http://localhost:8081")
+@FeignClient(name = "service-payment", url = "http://localhost:8082")
 public interface PaymentClient {
 
     @GetMapping("/payments/{id}")
---
+++ src/main/java/ie/atu/notification/service/PdfService.java
@@ -0,0 +1,16 @@
+package ie.atu.notification.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+
+    @Autowired
+    NotificationService notificationService;
+
+
+
+
+}
