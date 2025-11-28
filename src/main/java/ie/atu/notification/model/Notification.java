ca7aeecdacdb5524464597f188b3b2b87ac20180
Sighman <g00286398@atu.ie>
2025-11-28 22:15:11 +0000

    Adding Notification model

--- src/main/java/ie/atu/notification/model/Notification.java
+++ src/main/java/ie/atu/notification/model/Notification.java
@@ -1,4 +1,30 @@
 package ie.atu.notification.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "Notifications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Recipient email is required")
+    private String toEmail;
+
+    @NotBlank(message = "Subject is required")
+    private String subject;
+
+    @NotBlank(message = "Message is required")
+    private String message;
+
+    private boolean sent;
 }
